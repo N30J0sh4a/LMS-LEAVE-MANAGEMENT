799e1a39e59d196e665c29e839a240a4fb9033d5
Danilo Pelin <danilocanabanopelinjr@gmail.com>
2026-03-02 11:41:02 +0800

    Used a `.gitignore` node template to ignore irrelevant files

--- lms-employee-leave-sys/src/routes/index.tsx
+++ lms-employee-leave-sys/src/routes/index.tsx
@@ -1,13 +1,33 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { SidebarProvider, SidebarTrigger } from '../components/ui/sidebar'
+import { AppSidebar } from '../components/dashboard-sidebar';
+
+interface Props {
+  children?: React.ReactNode,
+}
+
+const Dashboard = ({children} : Props) => {
+  return (
+    //page content
+    <main className='flex flex-1 flex-col w-screen h-screen m-0 p-0'>
+      <section>
+        <SidebarProvider>
+          <main>
+            <AppSidebar/>
+            <SidebarTrigger/>
+            {children}
+          </main>
+        </SidebarProvider>
+      </section>
+      <section>
+
+      </section>
+    </main>
+  )
+}
+
+export default Dashboard;
 
 export const Route = createFileRoute('/')({
   component: Dashboard,
 })
-
-function Dashboard() {
-  return <main className='flex flex-1 flex-col w-screen h-screen m-0 p-0'>
-    
-  </main>
-}
-
-export default Dashboard;
